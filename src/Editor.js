4f798e4360f4dd06bc748e794642b30655ebfc0e
TiavinaMichael <ralainirinatiavina@gmail.com>
2021-08-22 11:23:44 +0300

    add footer

--- src/Editor.js
+++ src/Editor.js
@@ -5,12 +5,13 @@ import { jsx } from "@emotion/react";
 import ImageEditor from "@toast-ui/react-image-editor";
 
 import Button from "./components/Button";
-import { Upload } from "antd";
+import { Upload, Layout } from "antd";
+import Footer from "./components/Footer";
 
 const props = (image) => ({
   includeUI: {
     loadImage: {
-      path: image,
+      path: image || "me.jpg",
       name: "SampleImage"
     },
     menu: [],
@@ -28,9 +29,6 @@ const uploadProps = {
 
 const classes = {
   editor: {
-    "& #container": {
-      padding: 0
-    },
     "& .tui-image-editor-container": {
       overflow: "hidden"
     },
@@ -69,32 +67,38 @@ const Editor = () => {
 
   const crop = async () => {
     const editor = editorRef.current?.getInstance();
-    // const result = editor.setCropzoneRect(5 / 4);
-    // console.log('result', result);
+
+    editor.crop(editor.getCropzoneRect()).then(function () {
+      editor.stopDrawingMode();
+      // resizeEditor();
+    });
 
     // const result = await editor.crop({ top: 0, left: 0, width: 100, height: 100});
-    const rect = editor.getCropzoneRect();
+    // const rect = editor.getCropzoneRect();
     // const result = editor.crop(editor.getCropzoneRect());
-    console.log("rect", rect);
+    // console.log("rect", rect);
   };
 
   return (
-    <div css={classes.editor} className="justifyStart">
-      <ImageEditor ref={editorRef} {...props(image)} />
+    <div css={classes.editor} className="flexCenter">
+      <Layout.Content>
+        <ImageEditor ref={editorRef} {...props(image)} />
 
-      <div className="flexRow m-t-20">
-        <div css={classes.buttonConainter}>
-          <Button text="Flip X" onClick={handleFlipX} />
+        <div className="flexRow m-t-20">
+          <div css={classes.buttonConainter}>
+            <Button text="Flip X" onClick={handleFlipX} />
+          </div>
+          <div css={classes.buttonConainter}>
+            <Upload {...uploadProps} onChange={onUpload}>
+              <Button text="Click to Upload" />
+            </Upload>
+          </div>
+          <div css={classes.buttonConainter}>
+            <Button text="Crop" onClick={crop} />
+          </div>
         </div>
-        <div css={classes.buttonConainter}>
-          <Upload {...uploadProps} onChange={onUpload}>
-            <Button text="Click to Upload" />
-          </Upload>
-        </div>
-        <div css={classes.buttonConainter}>
-          <Button text="Crop" onClick={crop} />
-        </div>
-      </div>
+      </Layout.Content>
+      <Footer />
     </div>
   );
 };
